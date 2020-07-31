dd47d2ef4c5aeee0c4b6d4957475f4b0d1597967
ikyrie <mah.c5r@gmail.com>
2020-07-31 15:46:17 -0300

    Preparando o filme

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,5 +1,32 @@
 export class Carousel {
-    constructor() {
-        console.log('Carousel criado com sucesso')
+    constructor(indicadores, proximo, anterior, produtos) {
+        this.navegacao = document.querySelector(indicadores)
+        this.listaProdutos = document.querySelector(produtos)
+        this.proximo = document.querySelector(proximo)
+        this.anterior = document.querySelector(anterior)
+
+        this.indicadores = this.getListaIndicadores()
+        this.slides = this.getListaSlides()
+        this.tamanhoSlide = this.getTamanhoSlide()
+
+        this.preparaSlides()
+    }
+
+    getListaIndicadores() {
+        return Array.from(this.navegacao.children)
+    }
+
+    getListaSlides() {
+        return Array.from(this.listaProdutos.children)
+    }
+
+    getTamanhoSlide() {
+        return this.slides[0].getBoundingClientRect().width
+    }
+
+    preparaSlides() {
+        this.slides.forEach((slide, i) => {
+            slide.style.left = this.tamanhoSlide * i + 'px'
+        })
     }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 import { Carousel } from "./carousel.js";
 
-new Carousel()
+const indicadores = '[data-carousel-indicadores]'
+const proximo = '[data-proximo]'
+const anterior = '[data-anterior]'
+const produtos = '[data-carousel-produtos]'
+
+new Carousel(indicadores, proximo, anterior, produtos)
