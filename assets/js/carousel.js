6a5ef40fa65fd0616e6aaec777376f14b9ee62f8
ikyrie <mah.c5r@gmail.com>
2020-08-03 18:31:22 -0300

    Movimentacao

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,6 +8,12 @@ export class Carousel {
         this.indicadores = this.getListaIndicadores()
         this.slides = this.getListaSlides()
         this.tamanhoSlide = this.getTamanhoSlide()
+        this.slideAtual = this.getSlideAtual()
+
+        this.indiceDoSlideAtual = 0
+
+        this.proximo.addEventListener('click', this.proximoSlide.bind(this))
+        this.anterior.addEventListener('click', this.voltaSlide.bind(this))
 
         this.preparaSlides()
     }
@@ -22,6 +28,37 @@ export class Carousel {
 
     getTamanhoSlide() {
         return this.slides[0].getBoundingClientRect().width
+    }
+
+    getSlideAtual() {
+        return this.slides[this.indiceDoSlideAtual]
+    }
+
+    proximoSlide() {
+        let proximaPosicao = this.indiceDoSlideAtual + 1
+        if(proximaPosicao > this.slides.length - 1) {
+            proximaPosicao = proximaPosicao % this.slides.length
+        }
+        this.vaParaSlide(proximaPosicao)
+    }
+
+    voltaSlide() {
+        let posicaoAnterior = this.indiceDoSlideAtual - 1
+        if(posicaoAnterior < 0) {
+            posicaoAnterior = this.slides.length - 1
+        }
+        this.vaParaSlide(posicaoAnterior)
+    }
+
+    vaParaSlide(posicao) {
+        this.indiceDoSlideAtual = posicao
+        const proximoSlide = this.getSlideAtual()
+
+        this.scrollParaSlide(proximoSlide)
+    }
+
+    scrollParaSlide(slideSelecionado) {
+        this.listaProdutos.style.transform = 'translateX(-' + slideSelecionado.style.left + ')'
     }
 
     preparaSlides() {
